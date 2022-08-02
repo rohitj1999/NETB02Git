1eddc98e45bd320946ba72c1ea7d54f910337559
rohitj1999 <110378719+rohitj1999@users.noreply.github.com>
2022-08-02 20:46:16 +0530

    by Git Update

--- NETBGit/src/com/cjc/Demo.java
+++ NETBGit/src/com/cjc/Demo.java
@@ -6,6 +6,11 @@ public class Demo {
 	{
 		System.out.println("Mehtod m1");
 	}
+	
+	public void m2()
+	{
+		System.out.println("Mehtod=====m2");
+	}
             public static void main(String[] args) {
 	
 	 System.out.println("Hello");
