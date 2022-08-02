6386a85b0bc6f0b0bcf8e9b2b404f62789c95fbf
rohitj1999 <110378719+rohitj1999@users.noreply.github.com>
2022-08-02 19:49:02 +0530

    by Git

--- NETBGit/src/com/cjc/Demo.java
+++ NETBGit/src/com/cjc/Demo.java
@@ -2,8 +2,11 @@ package com.cjc;
 
 public class Demo {
 
-	
- public static void main(String[] args) {
+	public void m1()
+	{
+		System.out.println("Mehtod m1")
+	}
+            public static void main(String[] args) {
 	
 	 System.out.println("Hello");
 	 
